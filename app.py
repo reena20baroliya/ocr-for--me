51a9b282e831810403b983fc549c1898b203215c
reena20baroliya <88086440+reena20baroliya@users.noreply.github.com>
2021-07-29 13:40:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ img = st.file_uploader("choose an file")
 if img is not None:
   img_read = Image.open(img)
   st.image(img)
-  if st.button('PREDICT'):
-    op = pytesseract.image_to_string(img_read)
-    st.write(op)
+if st.button('PREDICT'):
+  op = pytesseract.image_to_string(img_read)
+  st.write(op)
